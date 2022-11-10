9d31778e4843b8c4de75947302d0837e9d3a3052
Fenneig <ninetailedkot92@gmail.com>
2022-11-10 06:31:19 +0300

    create input reader and setup player movement

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DefaultNamespace
+{
+    public class InputReader : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+
+        public void OnMovement(InputAction.CallbackContext context) =>
+            _player.MoveDirection = context.ReadValue<Vector2>();
+
+        public void OnAttack(InputAction.CallbackContext context) => _player.Attack();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _camera;
+    [SerializeField] private CharacterController _controller;
+    [Header("Settings")]
+    [SerializeField] private float _speed;
+    [SerializeField] private float _turnSmoothTime = 0.1f;
+
+    private Vector2 _moveDirection;
+
+    public Vector2 MoveDirection
+    {
+        set => _moveDirection = value;
+    }
+
+    private float _turnSmoothVelocity;
+
+    void Update()
+    {
+        Vector3 direction = new Vector3(_moveDirection.x, 0f, _moveDirection.y).normalized;
+        if (direction.magnitude > 0)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity,
+                _turnSmoothTime);
+            
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            _controller.Move(moveDirection * _speed * Time.deltaTime);
+        }
+    }
+
+    public void Attack()
+    {
+        Debug.Log("attacked");
+    }
+}
