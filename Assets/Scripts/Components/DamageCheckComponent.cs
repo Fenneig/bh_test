50570628d8064c9254386eedfe6ea2ddaa98d627
Fenneig <ninetailedkot92@gmail.com>
2022-11-11 06:17:27 +0300

    add test colliders interactions

---
+++ Assets/Scripts/Components/DamageCheckComponent.cs
@@ -0,0 +1,13 @@
+﻿using Character;
+using UnityEngine;
+
+namespace Components
+{
+    public class DamageCheckComponent : MonoBehaviour
+    {
+        public void ApplyDamage(GameObject target)
+        {
+            Debug.Log($"{gameObject.GetComponentInParent<Player>().PlayerName} hit {target.name}");
+        }
+    }
+}
---
+++ Assets/Scripts/Components/EnterTriggerComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Components
+{
+    public class EnterTriggerComponent : MonoBehaviour
+    {
+        [SerializeField] private string[] _tags;
+        [SerializeField] private EnterEvent _action;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            foreach (var tag in _tags)
+                if (other.gameObject.CompareTag(tag)) _action?.Invoke(other.gameObject);
+        }
+
+        [Serializable] public class EnterEvent : UnityEvent<GameObject> { }
+    }
+}
